cdd421f3bf5685ce8e3143eb3ae2baa7cdb4f183
Saul FRagua <saul.fragua1988gmail.com>
2025-09-03 23:45:39 -0500

    creando modulo de fotografia

--- frontend/src/app/modulos/fotografias/fotografias.ts
+++ frontend/src/app/modulos/fotografias/fotografias.ts
@@ -1,12 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HistorialProgreso } from './historial-progreso/historial-progreso';
+import { SubirFotografia } from './subir-fotografia/subir-fotografia';
+import { ComparacionFotografica } from './comparacion-fotografica/comparacion-fotografica';
 
 @Component({
   selector: 'app-fotografias',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule
+    , HistorialProgreso
+    , SubirFotografia
+    , ComparacionFotografica
+  ],
   templateUrl: './fotografias.html'
 })
 export class Fotografias {
+componenteActivo: 'subir' | 'historial' | 'comparacion' | null = null;
 
+  mostrarComponente(tipo: 'subir' | 'historial' | 'comparacion') {
+    this.componenteActivo = tipo;
+  }
+
+  volverBotones() {
+    this.componenteActivo = null;
+  }
 }
--- frontend/src/app/modulos/fotografias/subir-fotografia/subir-fotografia.ts
+++ frontend/src/app/modulos/fotografias/subir-fotografia/subir-fotografia.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-subir-fotografia',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class SubirFotografia {
 
+  // Creamos un evento que avisará al padre
+  @Output() volver = new EventEmitter<void>();
+
+  // Función que se llama al hacer clic en "VOLVER"
+  onVolver() {
+    this.volver.emit(); // Envía el evento al componente padre
+  }  
 }

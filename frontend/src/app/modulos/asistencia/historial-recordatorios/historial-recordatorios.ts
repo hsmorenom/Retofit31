9c5004365072868623606a080d4cd859b5acacd5
hsmorenom <sebas03272009@hotmail.com>
2025-10-22 17:51:17 -0500

    Creación subcomponente historial-recordatorios en asistencia

---
+++ frontend/src/app/modulos/asistencia/historial-recordatorios/historial-recordatorios.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historial-recordatorios',
+  imports: [],
+  templateUrl: './historial-recordatorios.html'
+})
+export class HistorialRecordatorios {
+
+}

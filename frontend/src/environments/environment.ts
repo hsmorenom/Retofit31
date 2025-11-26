ebed3fc48dfb62fb4217e4a01b80d6d2fe47fbdb
Saul FRagua <saul.fragua1988gmail.com>
2025-11-26 08:30:17 -0500

    Descripción apk generados

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,15 +1,7 @@
 export const environment = {
   production: false,
-<<<<<<< HEAD
   apiUrl: "http://localhost:8000/backend/",
   apiApi: "http://localhost:8000/backend/api/",
   apiFoto: "http://localhost:8000/backend/"
-
-=======
-  apiUrl: "http://localhost:8000/api/"
-  
->>>>>>> 9d7566c (Descripción apk generados)
 };
-
-
-
+  

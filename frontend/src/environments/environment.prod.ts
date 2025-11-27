98fb94848fa00a6b1ce7454248678c7951e14277
hsmorenom <sebas03272009@hotmail.com>
2025-11-26 19:23:29 -0500

    api

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://www.72.61.12.100.nip.io/api/",
-  apiAPI: "https://www.72.61.12.100.nip.io/api/api",
-  imgUrl: "https://www.72.61.12.100.nip.io/api/verImagen.php?archivo="
+  apiUrl: "https://72.61.12.100.nip.io/api/",
+  apiApi: "https://72.61.12.100.nip.io/api/api/",
+  apiFoto: "https://72.61.12.100.nip.io/api/"
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:8000/backend/",
-  apiApi:"http://localhost:8000/backend/api"
+   apiUrl: "http://localhost:8000/backend/",
+  apiApi: "http://localhost:8000/backend/api/",
+  apiFoto: "http://localhost:8000/backend/"
   
 };
 
